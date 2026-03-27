6289e5b4e94f25bbef17ce82a3acf3c3fc11bef7
bharathkumar57 <vtu26385@veltech.edu.in>
2026-03-27 22:00:26 +0530

    Time: 8 ms (68.24%), Space: 46.9 MB (73.99%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -1,29 +1,25 @@
 class ParkingSystem {
 
-    int big;
-    int medium;
-    int small;
+    int big, medium, small;
 
-    // Constructor
     public ParkingSystem(int big, int medium, int small) {
         this.big = big;
         this.medium = medium;
         this.small = small;
     }
 
-    // Method to add car
     public boolean addCar(int carType) {
-        if (carType == 1) { // Big
+        if (carType == 1) {
             if (big > 0) {
                 big--;
                 return true;
             }
-        } else if (carType == 2) { // Medium
+        } else if (carType == 2) {
             if (medium > 0) {
                 medium--;
                 return true;
             }
-        } else if (carType == 3) { // Small
+        } else if (carType == 3) {
             if (small > 0) {
                 small--;
                 return true;
